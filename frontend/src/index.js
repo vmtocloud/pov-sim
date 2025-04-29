6fcbd53ab9fc9020734299174b740b70eb0cbebb
Ryan Kelly <ryan@mac.lan>
2025-04-29 14:24:14 -0700

    Committer: Ryan Kelly <ryan.kelly@grafana.com> Changes to be committed: 	modified:   .DS_Store 	modified:   airlines/Dockerfile 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,33 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { matchRoutes } from 'react-router-dom';
+import { initializeFaro, createReactRouterV6DataOptions, ReactIntegration, getWebInstrumentations, } from '@grafana/faro-react';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+  url: 'https://faro-collector-prod-us-west-0.grafana.net/collect/07f23e611a0bb31c49f6a64da8de92c1',
+  app: {
+    name: 'Pov-Sim',
+    version: '1.0.0',
+    environment: 'production'
+  },
+  trackGeolocation: false,
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+
+    // React integration for React applications.
+    new ReactIntegration({
+      router: createReactRouterV6DataOptions({
+        matchRoutes,
+      }),
+    }),
+  ],
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
